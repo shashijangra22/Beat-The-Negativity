b87e8745752e59a1cd8a1abb279f5399d0943f67
Shashi Jangra <shashijangra@live.com>
2019-11-30 23:30:17 +0530

    Final Prototype

---
+++ Assets/DetectCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+
+    public LayerMask layer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.forward,out hit,1,layer)){
+            Destroy(hit.transform.gameObject);
+        }
+    }
+}
---
+++ Assets/FollowController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowController : MonoBehaviour
+{
+
+    public GameObject gameobject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=gameobject.transform.position;
+        transform.rotation=gameobject.transform.rotation;
+    }
+}
